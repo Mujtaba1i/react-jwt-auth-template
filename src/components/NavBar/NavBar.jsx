977088a8c0c5b67e1e411b66ffc5242af9d1bd19
Mujtaba1i <unmujt@gmail.com>
2026-01-04 11:44:53 +0300

    quick commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router'
+
+function NavBar() {
+  return (
+    <nav>
+        <p><Link to='/Sign-up'>Sign-Up</Link></p>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+function SignUpForm() {
+
+    const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+    const { username, password, confirmPassword } = formData;
+    
+    function handleChange(event){
+        setFormData({...formData,[event.target.name]:event.target.value})
+        
+    }
+    
+    async function handleSubmit (event){
+        event.preventDefault()
+        delete formData.confirmPassword
+        console.log(formData)
+        
+    }
+
+    const isFormInvalid = () => !(username && password && password === confirmPassword)
+
+  return (
+    <div>
+        <h1>Signup</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username: </label>
+            <input name="username" id="username" type="text" onChange={handleChange} value={username}/><br /><br />
+            
+            <label htmlFor="password">password: </label>
+            <input name="password" id="password" type="password" onChange={handleChange} value={password}/><br /><br />
+
+            <label htmlFor="confirm-password">confirm password: </label>
+            <input name="confirmPassword" id="confirm-password" type="password" onChange={handleChange} value={confirmPassword}/><br /><br />
+            
+            <button disabled={isFormInvalid()}>Sign Up</button><br /><br />
+            <button onClick={() => navigate('/')}>Cancel</button>
+
+        </form>
+    </div>
+  )
+}
+
+export default SignUpForm
